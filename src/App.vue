dc0022cf359ab98fcbfec129fb9a8fd7a78e5ecb
Web-Entwickler <pixpox21@bk.ru>
2023-06-01 14:57:08 +0200

    feat: add App.vue

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+import TheHeader from './components/TheHeader.vue';
+import TheFooter from './components/TheFooter.vue';
+</script>
+
+<template>
+  <TheHeader />
+  <main>
+    <RouterView />
+  </main>
+  <TheFooter />
+</template>
+
+<style>
+#app {
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+}
+</style>
